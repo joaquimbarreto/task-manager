d26ee69aad38c8c5eb30a8d687154842ab81fe21
Joaquim Barreto <joaquim@barreto.co>
2019-07-26 21:00:17 +0100

    react readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # task-manager
+
+A task management app.
+
+## Technologies
+
+### Back-end
+
+- Node.js
+- Express.js
+- Mongoose.js
+
+### Front-end
+
+- React.js
+
+### Database
+
+- MongoDB
+
+### Deployment
+
+- Heroku
+- mLab addon
--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,11 @@
 // CRUD create read update delete
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
+
+const id = new ObjectID();
 
 MongoClient.connect(
   connectionURL,
@@ -18,45 +19,53 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
+    // db.collection("users").findOne({ name: "Joaquim" }, (error, user) => {
+    //   if (error) {
+    //     return console.log("error in finding user");
+    //   }
+
+    //   console.log(user);
+    // });
+
     // db.collection("users").insertOne({
     //   name: "Joaquim",
     //   age: 43
     // });
 
-    db.collection("tasks").insertMany(
-      [
-        {
-          description: "Learn Ruby basics.",
-          completed: true
-        },
-        {
-          description: "Learn JavaScript basics.",
-          completed: true
-        },
-        {
-          description: "Learn React.js basics.",
-          completed: true
-        },
-        {
-          description: "Learn PostgreSQL basics.",
-          completed: true
-        },
-        {
-          description: "Learn Node.js basics.",
-          completed: false
-        },
-        {
-          description: "Learn MongoDB basics.",
-          completed: false
-        }
-      ],
-      (error, result) => {
-        if (error) {
-          return console.log("unable to insert taks");
-        }
+    // db.collection("tasks").insertMany(
+    //   [
+    //     {
+    //       description: "Learn Ruby basics.",
+    //       completed: true
+    //     },
+    //     {
+    //       description: "Learn JavaScript basics.",
+    //       completed: true
+    //     },
+    //     {
+    //       description: "Learn React.js basics.",
+    //       completed: true
+    //     },
+    //     {
+    //       description: "Learn PostgreSQL basics.",
+    //       completed: true
+    //     },
+    //     {
+    //       description: "Learn Node.js basics.",
+    //       completed: false
+    //     },
+    //     {
+    //       description: "Learn MongoDB basics.",
+    //       completed: false
+    //     }
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("unable to insert taks");
+    //     }
 
-        console.log(result.ops);
-      }
-    );
+    //     console.log(result.ops);
+    //   }
+    // );
   }
 );
