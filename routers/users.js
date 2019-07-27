f5f963b8587e58be18f570f27331b292772b28f2
Joaquim Barreto <joaquim@barreto.co>
2019-07-27 19:09:47 +0100

    jwt delete

--- routers/users.js
+++ routers/users.js
@@ -53,19 +53,6 @@ router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
-
 router.patch("/users/:id", async (req, res) => {
   const _id = req.params.id;
   const updates = Object.keys(req.body);
@@ -92,13 +79,10 @@ router.patch("/users/:id", async (req, res) => {
   }
 });
 
-router.delete("users/:id", async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
+    await req.user.remove();
+    res.send(req.user);
   } catch (error) {
     res.status(500).send();
   }
--- server/index.js
+++ server/index.js
@@ -6,25 +6,8 @@ require("../db/mongoose");
 const app = express();
 const port = process.env.PORT || 3001;
 
-// app.use((req, res, next) => {
-//   console.log(req.method, req.path);
-//   next();
-// });
-
 app.use(express.json());
 app.use(usersRouter);
 app.use(tasksRouter);
 
 app.listen(port, () => console.log("Listening on port " + port));
-
-const jwt = require("jsonwebtoken");
-
-const myFunction = async () => {
-  const token = jwt.sign({ _id: "abc123" }, "sshhhh", { expiresIn: "7 days" });
-  // console.log(token);
-
-  const data = jwt.verify(token, "sshhhh");
-  // console.log(data);
-};
-
-myFunction();
