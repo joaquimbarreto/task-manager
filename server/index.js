cc76b988ef1a38c9622226352668f4687416238d
Joaquim Barreto <joaquim@barreto.co>
2019-07-27 12:23:39 +0100

    routers

--- server/index.js
+++ server/index.js
@@ -1,75 +1,13 @@
 const express = require("express");
-const User = require("../models/user");
-const Task = require("../models/task");
+const usersRouter = require("../routers/users");
+const tasksRouter = require("../routers/tasks");
 require("../db/mongoose");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
-
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
-
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
-
-app.get("/tasks", (req, res) => {
-  try {
-    const tasks = Task.find({});
-    res.send(tasks);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
-
-app.get("/tasks/:id", (req, res) => {
-  const _id = req.params.id;
-  try {
-    const task = Task.findById(_id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
+app.use(usersRouter);
+app.use(tasksRouter);
 
 app.listen(port, () => console.log("Listening on port " + port));
