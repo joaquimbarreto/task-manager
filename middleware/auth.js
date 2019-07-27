dc1ba71d54d8da47eb752accac901be2650883f8
Joaquim Barreto <joaquim@barreto.co>
2019-07-27 18:51:59 +0100

    jwt logout

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,20 +4,20 @@ const User = require("../models/user");
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    console.log(token);
     const decoded = jwt.verify(token, "sshhhh");
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token
     });
-    console.log(user);
     if (!user) {
       throw new Error();
     }
+    req.token = token;
     req.user = user;
+    console.log(req.user);
     next();
   } catch (error) {
-    res.status(401).send({ error: "Please authenticates." });
+    res.status(401).send({ error: "Please authenticate." });
   }
 };
 
