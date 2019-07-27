7068fe5b46cb67f64c38a5fee635df4e9669d70a
Joaquim Barreto <joaquim@barreto.co>
2019-07-27 12:37:51 +0100

    updated User model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,24 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const User = mongoose.model("User", {
   name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lovercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid.");
+      }
+    }
+  },
+  password: {
     type: String,
     required: true,
     trim: true
