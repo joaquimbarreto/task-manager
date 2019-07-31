35f9ef3df5ff7f43d51e4f01e7cc27466b28c028
Joaquim Barreto <joaquim@barreto.co>
2019-07-31 10:01:39 +0100

    heroku postbuild

--- server/index.js
+++ server/index.js
@@ -10,5 +10,14 @@ app.use(express.json());
 app.use(usersRouter);
 app.use(tasksRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+
+}
+
 app.listen(port, () => console.log("Listening on port " + port));
 
