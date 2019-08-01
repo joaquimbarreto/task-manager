e42e2cdafa8b8faf0ecf859a128ab40ba2de3a1c
Joaquim Barreto <joaquim@barreto.co>
2019-08-01 15:38:51 +0100

    more user test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require("supertest");
 const app = require("../server/app");
+const User = require("../models/user");
+
+const userOne = {
+  name: "Tony DiNozzo",
+  email: "tony@ncis.org",
+  password: "12345678"
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
 
 test("Should signup a new user", async () => {
   await request(app)
@@ -10,4 +22,24 @@ test("Should signup a new user", async () => {
       password: "MyPass777!"
     })
     .expect(201);
+});
+
+test("Should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password
+    })
+    .expect(200);
+});
+
+test("Should not login nonexistent user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: "thisisnotmypass"
+    })
+    .expect(400);
 });
