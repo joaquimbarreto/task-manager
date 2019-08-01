7c93fedfe8180ca33448c2811075f8634567f1a0
Joaquim Barreto <joaquim@barreto.co>
2019-08-01 15:06:30 +0100

    Before npm install in client

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+// import ReactDOM from "react-dom";
+import { MemoryRouter } from "react-router-dom";
+import { mount } from "enzyme";
+import App from "./App";
+import Register from "./components/Register";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+// jest and enzyme
+test("valid path should not redirect to 404", () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={["/register"]}>
+      <App />
+    </MemoryRouter>
+  );
+  expect(wrapper.find(Register)).toHaveLength(1);
 });
--- server/index.js
+++ server/index.js
@@ -1,16 +1,10 @@
+const app = require("./app");
 const express = require("express");
-const usersRouter = require("../routers/users");
-const tasksRouter = require("../routers/tasks");
 const csp = require("helmet-csp");
 const path = require("path");
 require("../db/mongoose");
 
-const app = express();
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(usersRouter);
-app.use(tasksRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
