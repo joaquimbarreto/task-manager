3ab27a46b0eb9bcca2361ba74ae5759fe7044ced
Joaquim Barreto <joaquim@barreto.co>
2019-08-02 22:09:11 +0100

    server/index.js with /*

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
